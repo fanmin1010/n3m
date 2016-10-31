e8c9e26286451c23445af837b75c244c462cf817
Michael Makris <mm3443@columbia.edu>
2016-10-31 02:25:52 +0000

    added models for FriendMessage, Party, PartyUser and PartyMessage

--- application/models.py
+++ application/models.py
@@ -41,3 +41,66 @@ class Friendship(db.Model):
         self.friendee = friendee
     def __repr__(self):
         return '<Friendship between %r and %r>' % (self.friender, self.friendee)
+
+class FriendMessage(db.Model):
+    fmID = db.Column(db.Integer(), primary_key = True, nullable = False)
+    fs_id = db.Column(db.Integer(), db.ForeignKey('friendship.fs_id'), nullable=False)
+    senderID = db.Column(db.Integer(), db.ForeignKey('user.id'), nullable=False)
+    timestamp = db.Column(db.DateTime(), nullable=False, server_default=db.func.now())
+    message = db.Column(db.String(65535), nullable=False)
+
+    def __init__(self, fs_id, senderID, timestamp, message):
+        self.fs_id = fs_id
+        self.senderID = senderID
+        self.message = message
+
+    @staticmethod
+    def getFriendMessages(senderID)
+        friendMessages = FriendMessage.query.filter_by(senderID=senderID)
+        return friendMessages
+
+class Party(db.Model):
+    partyID = db.Column(db.Integer(), primary_key = True, nullable=False)
+    partyName = db.Column(db.String(255), nullable=False)
+    ownerID = db.Column(db.Integer(), db.ForeignKey('user.id'), nullable=False)
+
+    def __init__(self, partyName, ownerID):
+        self.partyname = partyName
+        self.ownerID = ownerID
+
+    @staticmethod
+    def getMyParties(ownerID):
+        parties = Party.query.filter_by(ownerID=ownerID)        
+        return parties
+
+class PartyUser(db.model):
+    puID = db.Column(db.Integer(), primary_key = True, nullable=False)
+    partyID = db.Column(db.Integer(), db.ForeignKey('party.partyID'), nullable=False)
+    userID = db.Column(db.Integer(), db.ForeignKey('user.id'), nullable=False)
+
+    def __init__(self, partyID, userID):
+        self.partyID = partyID
+        self.userID = userID
+
+    @staticmethod
+    def getPartyUsers(partyID):
+        partyUsers = PartyUser.query.filter_by(partyID=partyID)
+        return partyUsers
+
+class PartyMessage(db.Model):
+    pmID = db.Column(db.Integer(), primary_key = True, nullable=False)
+    partyID = db.Column(db.Integer(), db.ForeignKey('party.partyID'), nullable=False)
+    senderID = db.Column(db.Integer(), db.ForeignKey('user.id'), nullable=False)
+    timestamp = db.Column(db.DateTime(), nullable=False, server_default=db.func.now())
+    message = db.Column(db.String(65535), nullable=False)
+
+    def __init__(self, partyID, senderID, timestamp, message):
+        self.partyID = partyID
+        self.senderID = senderID
+        self.message = message
+
+    @staticmethod
+    def getPartyMessages(partyID):
+        partyMessages = PartyMessage.query.filter_by(partyID=partyID)
+        return partyMessages
+
