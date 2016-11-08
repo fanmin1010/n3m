e2724a3274096b336d95f1a0efe8467580caadb9
Michael Makris <mm3443@columbia.edu>
2016-11-08 19:30:53 +0000

    added more coverage

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,6 +1,7 @@
 from testing_config import BaseTestConfig
 from application.models import User
-
+from application.models import Party
+from application.models import PartyUser
 
 class TestModels(BaseTestConfig):
     def test_get_user_with_email_and_password(self):
@@ -8,4 +9,14 @@ class TestModels(BaseTestConfig):
                 User.get_user_with_email_and_password(
                         self.default_user["email"],
                         self.default_user["password"])
+        )
+
+    def test_getMyParties(self):
+        self.assertTrue(
+                Party.getMyParties(self.default_user["id"])
+        )
+
+    def test_getPartyUsers(self):
+        self.assertTrue(
+                PartyUser.getPartyUsers(self.default_party["partyID"])
         )
