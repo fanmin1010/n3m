6cc66dd3cd8a39a43db7f81348ea8f332f3b792a
Michael Makris <mm3443@columbia.edu>
2016-11-08 19:46:22 +0000

    more coverage

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,6 +2,7 @@ from testing_config import BaseTestConfig
 from application.models import User
 from application.models import Party
 from application.models import PartyUser
+from application.models import PartyMessage
 
 class TestModels(BaseTestConfig):
     def test_get_user_with_email_and_password(self):
@@ -19,4 +20,14 @@ class TestModels(BaseTestConfig):
     def test_getPartyUsers(self):
         self.assertTrue(
                 PartyUser.getPartyUsers(self.default_party["partyID"])
+        )
+
+    def test_PartyMessages(self):
+        self.assertTrue(
+                PartyMessage(self.default_party["partyID"],self.default_user["id"],"2016-11-08 00:00:00","My Message")
+        )
+
+    def test_getPartyMessages(self):
+        self.assertTrue(
+                PartyMessage.getPartyMessages(self.default_party["partyID"])
         )
