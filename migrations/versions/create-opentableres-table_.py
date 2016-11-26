ecb443d6489dc5e29d6458f39d85825a297c6579
Michael Makris <mm3443@columbia.edu>
2016-11-26 03:41:01 +0000

    uber and opentable migrations

---
+++ migrations/versions/create-opentableres-table_.py
@@ -0,0 +1,31 @@
+"""empty message
+
+Revision ID: create-opentableres-table
+Revises: create-uber-table
+Create Date: 2016-11-26 02:56:19.519663
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'create-opentableres-table'
+down_revision = 'create-uber-table'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.create_table('opentable',
+    sa.Column('id', sa.Integer(), primary_key=True),
+    sa.Column('userID', sa.Integer(), sa.ForeignKey('user.id'), nullable=False),
+    sa.Column('timestamp', sa.DateTime, server_default=sa.func.current_timestamp()),
+    sa.Column('date', sa.Date()),
+    sa.Column('timeslot', sa.String(length=255), nullable=False),
+    sa.Column('destination', sa.String(length=255), nullable=False),
+    sa.Column('partysize', sa.Integer(), nullable=False),
+    sa.PrimaryKeyConstraint('id')
+    )
+
+
+def downgrade():
+    op.drop_table('opentable')
---
+++ migrations/versions/create-uber-table_.py
@@ -0,0 +1,31 @@
+"""empty message
+
+Revision ID: create-uber-table
+Revises: user-mkae-username-uniq
+Create Date: 2016-11-26 02:55:46.699540
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'create-uber-table'
+down_revision = 'user-mkae-username-uniq'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+        op.create_table('uber',
+        sa.Column('id', sa.Integer(), primary_key=True),
+        sa.Column('userID', sa.Integer(), sa.ForeignKey('user.id'), nullable=False),
+        sa.Column('timestamp', sa.DateTime, server_default=sa.func.current_timestamp()),
+        sa.Column('duration', sa.Interval()),
+        sa.Column('location', sa.String(length=255), nullable=False),
+        sa.Column('destination', sa.String(length=255), nullable=False),
+        sa.Column('cost', sa.Float(), nullable=False),
+        sa.PrimaryKeyConstraint('id')
+        )
+
+
+def downgrade():
+    op.drop_table('uber')
